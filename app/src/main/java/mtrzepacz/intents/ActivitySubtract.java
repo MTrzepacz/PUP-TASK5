869f6bb9b6220694a4adf421cd7cc887fe1e0e36
Mateusz.T <mateusz.trzepacz@gmail.com>
2017-12-26 12:19:34 +0100

    finished

---
+++ app/src/main/java/mtrzepacz/intents/ActivitySubtract.java
@@ -0,0 +1,28 @@
+package mtrzepacz.intents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ActivitySubtract extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        Bundle bundle = getIntent().getExtras();
+        Integer arg1 = bundle.getInt(MainActivity.PACKAGE_NAME + getResources().getString(R.string.arg_1_name),0);
+        Integer arg2 = bundle.getInt(MainActivity.PACKAGE_NAME + getResources().getString(R.string.arg_2_name),0);
+
+        Integer result = arg1 - arg2;
+        Toast.makeText(getApplicationContext(),"substract activity", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent();
+        intent.putExtra(getResources().getString(R.string.result),result);
+        intent.putExtra(getResources().getString(R.string.operation), getResources().getString(R.string.subtract));
+        setResult(RESULT_OK, intent);
+
+        super.finish();
+    }
+}
